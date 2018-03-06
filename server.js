bf535f5fe81317a02724b80a315eb73f41acaf37
Yingda Luo <yluo@medallia.com>
2018-03-05 22:23:22 -0800

    czxzxzx zxczx

--- server.js
+++ server.js
@@ -18,4 +18,27 @@ app.post('/facebook_login', function (req, res) {
   });
 });
 
+app.post('/facebook_send_message', function (req, res) {
+  var facebookAppState = req.body.facebookAppState
+  var message = req.body.message
+  var targetUsers = req.body.targetUsers
+
+  login({appState: JSON.parse(facebookAppState)}, (err, api) => {
+    if (err) return res.status(401).send({error: 'failed to login!'});
+    targetUsers.forEach(function(targetUser) {
+      api.getUserID(targetUser, (err, data) => {
+          if(err) return res.status(500).send({error: 'sending failed!'});
+
+          // Send the message to the best match (best by Facebook's criteria)
+          var msg = {
+            body: message
+          }
+          var threadID = data[0].userID;
+          api.sendMessage(msg, threadID);
+      });
+    });
+    res.status(200).send();
+  });
+});
+
 app.listen(server_port);
